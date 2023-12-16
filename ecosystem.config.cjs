e0223e0827647e88af573055b579dbb00ed318a3
mohamed Ragheb <MohamedRagheb@geexar.dev>
2023-12-16 11:13:17 +0200

    !

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'aljoodmed-t',
-      exec_mode: 'fork',
-      instances: 1,
+      exec_mode: 'cluster',
+      instances: "max",
       script: './.output/server/index.mjs',
       args: 'start',
       env: {
